73250099055a8899f20dc7480cdd5e7a3a04fb6b
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-04-08 12:53:23 -0500

    cars

---
+++ Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties_LastName_FirstName
+{
+    internal class Car
+    {
+
+        private string model;
+
+        public string Model { get { return model; } set { model = value; } };
+
+        public string Make { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car car = new Car();
+            car.Make = "Ford";
+            car.Model = "Mustang";
+
+            Console.WriteLine($"Make: {car.Make}, Model: {car.Model}");
+
+            Car car2 = new Car();
+            car2.Make = "Chevy";
+            car2.Model = "Camero";
+
+            Console.WriteLine($"Make: {car2.Make}, Model: {car2.Model}");
+
+            Car car3 = new Car();
+            Console.WriteLine($"Make: {car3.Make}, Model: {car3.Model}");
         }
     }
 }
