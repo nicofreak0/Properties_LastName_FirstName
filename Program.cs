878deab8da97bbb1ffaed443c0c3deefd1948def
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-04-08 12:36:57 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Properties_LastName_FirstName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
